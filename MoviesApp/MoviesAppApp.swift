d4d12d2083dcf5f73332377b6d94d95ccf5382d2
berlinsk <berliniumyar@gmail.com>
2025-09-11 16:47:29 +0200

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Берлинский Ярослав Владленович on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
