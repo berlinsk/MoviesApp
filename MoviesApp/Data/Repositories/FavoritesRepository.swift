803566d0607f36d294e480888641d5f2e9ec13fc
berlinsk <berliniumyar@gmail.com>
2025-09-12 06:12:46 +0200

    move UserDefaultsRepository to persistence layer

--- MoviesApp/Data/Repositories/FavoritesRepository.swift
+++ MoviesApp/Data/Repositories/FavoritesRepository.swift
@@ -14,32 +14,3 @@ protocol FavoritesRepository: AnyObject {
     func isFavorite(id: Int) -> Bool
     func allIDs() -> [Int]
 }
-
-final class UserDefaultsFavoritesRepository: FavoritesRepository, ObservableObject {
-    private let key = "favorites_ids"
-    private let defaults: UserDefaults
-
-    @Published private var ids: [Int] = []
-    var idsPublisher: Published<[Int]>.Publisher { $ids }
-
-    init(defaults: UserDefaults = .standard) {
-        self.defaults = defaults
-        self.ids = (defaults.array(forKey: key) as? [Int]) ?? []
-    }
-
-    func toggle(id: Int) {
-        if ids.contains(id) {
-            ids.removeAll { $0 == id }
-        } else {
-            ids.append(id)
-        }
-        defaults.set(ids, forKey: key)
-    }
-
-    func isFavorite(id: Int) -> Bool {
-        ids.contains(id)
-    }
-    func allIDs() -> [Int] {
-        ids
-    }
-}
